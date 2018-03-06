c2572a3fd2e5eb1cb31e9ab669218edd37303089
Ryota Nishi <nishi@swlab.cs.okayama-u.ac.jp>
2018-03-06 17:23:11 +0900

    Fix bug

--- show_all_file_extension.sh
+++ show_all_file_extension.sh
@@ -3,7 +3,7 @@
 reg_fname=".*\/(.*)"
 reg_ext="[^.].*\.([^.]+$)"
 
-for file in `\find $1 -type f -regextype posix-basic -not -regex ".*\/\..*"`; do
+for file in `\find $1 -type f -not -regex ".*\/\..*"`; do
     if [[ $file =~ $reg_fname ]]
     then
 	if [[ ${BASH_REMATCH[1]} =~ $reg_ext ]]
